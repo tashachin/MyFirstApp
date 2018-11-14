f0f1b0f8920e558dd48ecadb1d8872bb6ffac65f
khemeioa <tasha.s.chin@gmail.com>
2018-11-13 17:15:37 -0800

    TODO: Fix second activity so it displays results from first activity.

--- app/src/main/java/com/example/tasha_pc/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/tasha_pc/myfirstapp/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.tasha_pc.myfirstapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.tasha_pc.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
